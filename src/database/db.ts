064b828155e8f5bdc9d4f200943d7991d93532af
rphlmr <raphael@uaerom.com>
2024-07-08 14:32:39 +0200

    fix

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,7 +2,9 @@ import { PGlite } from "@electric-sql/pglite";
 import { drizzle } from "drizzle-orm/pglite";
 import * as schema from "database/schema";
 
-const client = new PGlite("src/database/data.db");
+const client = new PGlite();
+// Uncomment to use a local db on your machine. This does not work with StackBlitz.
+// const client = new PGlite("src/database/data.db");
 const db = drizzle(client, { schema });
 
 export { db, client };
